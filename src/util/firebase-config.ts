d46433703c2f3b2f858ce51a2badc1cbd39034c9
Schmell Mafeet <sheldon.street@gmail.com>
2025-02-18 09:36:49 -0500

    remove key

--- src/util/firebase-config.ts
+++ src/util/firebase-config.ts
@@ -8,18 +8,9 @@ import {
 } from "firebase/firestore";
 import { getStorage, connectStorageEmulator } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7SFX9Qyr_b4AMGlM6bAUiG3BZBrh72B8",
-//   authDomain: "schmell-first-try.firebaseapp.com",
-//   databaseURL: "https://schmell-first-try-default-rtdb.firebaseio.com",
-//   projectId: "schmell-first-try",
-//   storageBucket: "schmell-first-try.appspot.com",
-//   messagingSenderId: "464776014382",
-//   appId: "1:464776014382:web:8667afbe5dec4c4408cdbf",
-//   measurementId: "G-Q077CT5BYW"
-// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCbGouPxwVKd5GkLJAoNGMhIIlDz2CViw4",
+  apiKey: "---",
   authDomain: "otw-scoring.firebaseapp.com",
   projectId: "otw-scoring",
   storageBucket: "otw-scoring.appspot.com",
